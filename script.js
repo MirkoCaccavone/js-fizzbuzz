c02f290649ddca52741730f31ca1a8f983af88ac
Mirko <mirkocaccavone03@gmail.com>
2024-12-18 13:51:20 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Scrivi un programma che stampi i numeri da 1 a 100, 
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e 
+// per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// FUNZIONE
+
+// ciclo che stampa numeri da 1 a 100
+
+// controllare se un numero e divisibile per 3 0 5
+
+// stampare fizz invece del 3 o per i suoi multipli
+
+// stampare buzz invece del 5 o per i suoi multipli
+
+// OUTPUT
+// visualizzare lista completa
+
+
+
+
